3b7b728dae9929e318760a0decf47e3d41b3e7cd
Alex <alexchenchen1@gmail.com>
2020-12-30 00:01:49 -0800

    processed raw data

--- Parsed/classify.py
+++ Parsed/classify.py
@@ -6,7 +6,7 @@ folder_dict = {'a':"Apple", 'ch':"Chip", 'mh':"MermaidHair", 'nf': "NormieFish",
 folder_iter = {'a':0, 'ch':0, 'mh':0, 'nf':0, 'uh':0, "b":0, 'c':0, 'ds':0, 'ec':0, 'ef':0, 'gf':0, 'lp':0, 'ws':0, 'ze':0, 'r':0}
 
 for key, value in folder_dict.items():
-    folder_iter[key] = len(os.listdir(os.path.join("images", value)))
+    folder_iter[key] = len(os.listdir(value))
 
 # Start main loop
 for f in os.listdir():
@@ -14,8 +14,9 @@ for f in os.listdir():
         print("open -gj {}".format(f))
         subprocess.run("open {}".format(f), shell=True)
         selection = input()
-        if folder_dict[selection] in os.listdir("images"):
-            os.rename(f, os.path.join("images", folder_dict[selection], folder_dict[selection]+"_"+str(folder_iter[selection])+".png"))
+        if folder_dict[selection] in os.listdir():
+            os.rename(f, os.path.join(folder_dict[selection], folder_dict[selection]+"_"+str(folder_iter[selection])+".png"))
+            folder_iter[selection] += 1
         else:
             print("BAD")
             break
