1d718e6547593a10bcf8cbacfd1734c98ced744f
Alex <alexchenchen1@gmail.com>
2020-12-29 21:28:59 -0800

    Fixed pruner, pruned images.

--- Parsed/pruner.py
+++ Parsed/pruner.py
@@ -6,17 +6,18 @@ folder_iter = {'Apple':0, 'Chip':0, 'MermaidHair':0, 'NormieFish':0, 'UnicornHor
 
 for key, value in folder_iter.items():
     files = set(os.listdir(os.path.join("images", key)))
+    to_remove = set()
     for f in files:
         if f[:3] == "img":
-            files.remove(f)
+            to_remove.add(f)
+    for r in to_remove:
+        files.remove(r)
 
     folder_iter[key] = len(files)
-     
-
 
 # Start main loop
 for d in os.listdir("images"):
     for f in os.listdir(os.path.join("images", d)):
-        if f[-3:] == "img":
+        if f[:3] == "img":
             os.rename(os.path.join("images", d, f), os.path.join("images", d, d+"_"+str(folder_iter[d])+".png"))
             folder_iter[d] += 1
