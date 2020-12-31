c16c59b1e7dfe87b67675e1c0b8da03d793399ec
achenchen1 <alexchenchen1@gmail.com>
2020-12-30 19:22:32 -0500

    New raw images

--- Parsed/curl.py
+++ Parsed/curl.py
@@ -1,6 +1,6 @@
 import subprocess
 
-with open('links2.txt', "r") as f:
+with open('list3.txt', "r") as f:
 	i = 0
 	for line in f:
 		subprocess.run("curl -o images/img_{}.png {}".format(i, line), shell=True)
--- bot/test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for d in os.listdir():
-    if not os.path.isdir(d):
-        continue
-    for f in os.listdir(d):
-        if f[-3:] == 'txt':
-            os.rename(os.path.join(d, f), f)
